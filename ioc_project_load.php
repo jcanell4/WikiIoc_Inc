3564c5fd62472e1109d16b525d29f964a1bafb7d
rclaver <rclaver@xtec.cat>
2016-07-05 13:20:57 +0200

    añadir carga de clases principales en el autoload

--- ioc_project_load.php
+++ ioc_project_load.php
@@ -13,24 +13,54 @@ spl_autoload_register('ioc_project_autoload');
 
 function ioc_project_autoload($name) {
     global $plugin_controller;
+    static $classes = null;
+    static $defaultClasses = null;
+
     /*
-     * La ruta y nombre del fichero se compone de:
-     * - una ruta base + 
-     * - el directorio del proyecto + 
-     * - el directorio del tipo de clase (indicado en el archivo de configuración del proyecto) +
-     * - el nombre de la clase
+     * Carga la clase solicitada del conjunto de clases principales del proyecto.
+     * Si no existe la clase solicitada específica en el proyecto,
+     * la busca en el proyecto por defecto
      */
     if ($plugin_controller) {
         $projectDir = DOKU_PROJECTS.$plugin_controller->getCurrentProject();
+
+        if (is_null($classes)) {
+            $classes = getMainClass($projectDir);
+        }
+        if (is_null($defaultClasses)) {
+            $defaultClasses = getDefaultMainClass();
+        }
+
+        if (isset($classes[$name])) {
+            if (@file_exists($classes[$name]) && is_file($classes[$name])) {
+                require_once($classes[$name]);
+                return;
+            }elseif (isset($defaultClasses[$name])) {
+                if (@file_exists($defaultClasses[$name]) && is_file($defaultClasses[$name])) {
+                    require_once($defaultClasses[$name]);
+                    return;
+                }
+            }
+        }
+    }
+    
+    if ($plugin_controller) {
+        //$projectDir = DOKU_PROJECTS.$plugin_controller->getCurrentProject();
         $defaultClassCfg = $projectDir."projectClassCfg.php";
         $existsDefaultClassCfg = @file_exists($defaultClassCfg);
-
+        /*
+         * La ruta y nombre del fichero que contiene la classe solicitada se compone de:
+         * - una ruta base + 
+         * - el directorio del proyecto + 
+         * - el directorio del tipo de clase (indicado en el archivo de configuración del proyecto) +
+         * - el nombre de la clase
+         */
         $type_class = splitCamelCase($name, "last");
         if ($type_class) {
             $class_dir = getClassDir($type_class);
             foreach ($class_dir as $dir) {
                 $class_file = $projectDir."/".$dir."/".$name.".php";
-                if ($class_file && file_exists($class_file) && is_file($class_file)) {
+                if ($class_file && @file_exists($class_file) && is_file($class_file)) {
                     include_once ($class_file);
                     break;
                 }else if($existsDefaultClassCfg) {
@@ -38,7 +68,7 @@ function ioc_project_autoload($name) {
                     $arr_default_class_dir = projectClassCfg::getDefaultClassDir($type_class);
                     foreach ($arr_default_class_dir as $dir) {
                         $fichero = $dir."/".$name.".php";
-                        if (file_exists($fichero) && is_file($fichero)) {
+                        if (@file_exists($fichero) && is_file($fichero)) {
                             include_once ($fichero);
                             break 2;
                         }
@@ -64,6 +94,14 @@ function getClassDir($name) {
                        )
           );
    return $cfg[$name];
+}
+
+function getMainClass($projectDir) {
+    $classes = array(
+                  "DokuModelAdapter" => "${projectDir}DokuModelAdapter.php"
+                 ,"DokuModelManager" => "${projectDir}DokuModelManager.php"
+               );
+    return $classes;
 }
 
 function splitCamelCase($name, $elem) {
