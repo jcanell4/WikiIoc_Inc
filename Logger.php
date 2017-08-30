be507c33f387049b62a6088fea8af667750e35ed
jcanell4 <jcanell4@xtec.cat>
2017-08-31 00:31:31 +0200

    merge T066 amb master

--- Logger.php
+++ Logger.php
@@ -1,15 +1,17 @@
 <?php
 if(!defined('DOKU_INC')) die();
 /**
- * Description of Logger
- *
+ * Modo de Uso en un fichero (con valores por defecto estándar):
+ *    include_once(DOKU_INC."inc/inc_ioc/Logger.php");
+ *    function f() {
+ *        Logger::debug($TextoDescriptivo+ValoresVariables, $NúmError=0, __LINE__, __FILE__, $level=-1, $append=TRUE);
  * @author josep
  */
 class Logger {
     private static $fileAppend=FALSE;
     private static $fileName;
     private static $debugLevel=0;
-    
+
     public static function init($debugLevel=0, $filename=NULL, $fileAppend=FALSE) {
         self::$debugLevel=$debugLevel;
         self::$fileName = $filename;
@@ -18,17 +20,16 @@ class Logger {
 
     public static function debug($message, $err, $line, $file, $level=1, $append=NULL) {
         if (self::$debugLevel < $level) return;
-        if($append===NULL){
-            $append = self::$fileAppend;
-        }
+        $append = ($append===NULL) ? self::$fileAppend : ($append) ? $append=FILE_APPEND : $append;
+
         if(self::$fileName===NULL){
             $debugFile = DOKU_INC.'lib/plugins/tmp/debug.log';
         }else{
             $debugFile = self::$fileName;
         }
         msg($message, $err, $line, $file);
-        $tag = $err===0?"Info: ":"Error($err): ";
+        $tag = ($err===0) ? "Info" : "Error($err): ";
         $date = date("d-m-Y H:i:s");
         file_put_contents($debugFile, "$date ($tag)=> $message ($file:$line)\n", $append);
-    }    
+    }
 }
