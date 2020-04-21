10b14425c4d7ec9e6c72e30733b14ec371b9f447
rclaver <rclaver@xtec.cat>
2020-04-21 19:18:06 +0200

    corrección básica

--- ioc_project_load.php
+++ ioc_project_load.php
@@ -20,105 +20,113 @@ function ioc_project_autoload($name) {
         return false;
     }
 
-    /*
-     * Carga, si existen, las clases por defecto definidas en el proyecto.
-     */
+    // Carga, si existen, las clases por defecto definidas en el proyecto.
     if ($plugin_controller) {
         //Filtro previo
         $type_class = splitCamelCase($name, "last");
         if ($type_class) $arr_dir_class = getDirClass($type_class);
         if (!$type_class || !$arr_dir_class) return;
 
-        $existDokuModelManager = class_exists('DokuModelManager', FALSE);
-        $projectDir = $plugin_controller->getProjectTypeDir($plugin_controller->getCurrentProject());
+        if (($project = $plugin_controller->getCurrentProject())) {
+            $projectDir = $plugin_controller->getProjectTypeDir($project);
 
-        if (!$existDokuModelManager) {
-            include_once $projectDir.DOKUMODELMANAGER;
-        }
+            $existDokuModelManager = class_exists('DokuModelManager', FALSE);
+            if (!$existDokuModelManager) {
+                include_once $projectDir.DOKUMODELMANAGER;
+            }
 
-        if (is_null($defClasssProj)) {
-           $defClasssProj = DokuModelManager::getDefaultMainClass();
-        }
-        if (is_null($defClasses)) {
-            $defClasses = getMainClass($projectDir);
-        }
-
-        /*
-        // En el DokuModelManager de cada proyecto se establecen las rutas a las clases que necesita el proyecto
-        if ($existDokuModelManager) {
-            if ($dokuModelManager)
-                include_once $dokuModelManager;
             if (is_null($defClasssProj)) {
-                $defClasssProj = DokuModelManager::getDefaultMainClass();
+               $defClasssProj = DokuModelManager::getDefaultMainClass();
             }
-        }
-        if (is_null($defClasses)) {
-            $defClasses = getMainClass($projectDir);
-        }*/
-
-        //Aquí se averigua (y, en su caso, se carga) si se ha solicitado una Clase principal
-        if (isset($defClasssProj[$name]) &&
-            @file_exists($defClasssProj[$name]) &&
-            is_file($defClasssProj[$name])) {
-                require_once($defClasssProj[$name]);
-                return;
-        }elseif (isset($defClasses[$name]) &&
-            @file_exists($defClasses[$name]) &&
-            is_file($defClasses[$name])) {
-                require_once($defClasses[$name]);
-                return;
-        }
-
-        /*
-         * La ruta y nombre del fichero que contiene la classe solicitada se compone de:
-         * - una ruta base +
-         * - el directorio del proyecto +
-         * - el directorio del tipo de clase (indicado en el archivo de configuración del proyecto) +
-         * - el nombre de la clase
-         */
-        foreach ($arr_dir_class as $dir) {
-            $class_file = $projectDir.$dir.$name.".php";
-            //Busca la clase en las rutas propias del proyecto
-            if (@file_exists($class_file) && is_file($class_file)) {
-                include_once ($class_file);
-                return;
+            if (is_null($defClasses)) {
+                $defClasses = getMainClass($projectDir);
             }
-        }
-        //Si no encuentra la clase solicitada en las rutas propias del proyecto, buscará en rutas alternativas definidas en DokumodelManager
-        if ($existDokuModelManager) {
-            $arr_project_dir_class = DokuModelManager::getDefaultDirClass($type_class);
-            if ($arr_project_dir_class) {
-                foreach ($arr_project_dir_class as $projdir) {
-                    $fichero = $projdir.$name.".php";
-                    if (@file_exists($fichero) && is_file($fichero)) {
-                        include_once ($fichero);
-                        return;
+
+            /*
+            // En el DokuModelManager de cada proyecto se establecen las rutas a las clases que necesita el proyecto
+            if ($existDokuModelManager) {
+                if ($dokuModelManager)
+                    include_once $dokuModelManager;
+                if (is_null($defClasssProj)) {
+                    $defClasssProj = DokuModelManager::getDefaultMainClass();
+                }
+            }
+            if (is_null($defClasses)) {
+                $defClasses = getMainClass($projectDir);
+            }*/
+
+            //Aquí se averigua (y, en su caso, se carga) si se ha solicitado una Clase principal
+            if (isset($defClasssProj[$name]) &&
+                @file_exists($defClasssProj[$name]) &&
+                is_file($defClasssProj[$name])) {
+                    require_once($defClasssProj[$name]);
+                    return;
+            }elseif (isset($defClasses[$name]) &&
+                @file_exists($defClasses[$name]) &&
+                is_file($defClasses[$name])) {
+                    require_once($defClasses[$name]);
+                    return;
+            }
+
+            /*
+             * La ruta y nombre del fichero que contiene la classe solicitada se compone de:
+             * - una ruta base +
+             * - el directorio del proyecto +
+             * - el directorio del tipo de clase (indicado en el archivo de configuración del proyecto) +
+             * - el nombre de la clase
+             */
+            foreach ($arr_dir_class as $dir) {
+                $class_file = $projectDir.$dir.$name.".php";
+                //Busca la clase en las rutas propias del proyecto
+                if (@file_exists($class_file) && is_file($class_file)) {
+                    include_once ($class_file);
+                    return;
+                }
+            }
+            //Si no encuentra la clase solicitada en las rutas propias del proyecto, buscará en rutas alternativas definidas en DokumodelManager
+            if ($existDokuModelManager) {
+                $arr_project_dir_class = DokuModelManager::getDefaultDirClass($type_class);
+                if ($arr_project_dir_class) {
+                    foreach ($arr_project_dir_class as $projdir) {
+                        $fichero = $projdir.$name.".php";
+                        if (@file_exists($fichero) && is_file($fichero)) {
+                            include_once ($fichero);
+                            return;
+                        }
                     }
                 }
             }
-        }
 
-        $matches = [];
-        if (preg_match('/(.*)(Authorization)$/', $name, $matches)) {
-            if (is_file(DOKU_LIB_IOC."wikiiocmodel/authorization/{$matches[0]}.php")) {
-                require_once(DOKU_LIB_IOC."wikiiocmodel/authorization/{$matches[0]}.php");
-                return;
-            }
-        }
+            //Solicita la carga la clase de autorización por defecto
+            if (getAuthorizationClass($name)) return;
 
-        //Si todavía no la encuentra, buscará la clase en las rutas de la raíz WIKI_IOC_MODEL
-        foreach ($arr_dir_class as $dir) {
-            $class_file = WIKI_IOC_MODEL.$dir.$name.".php";
-            if (@file_exists($class_file) && is_file($class_file)) {
-                include_once ($class_file);
-                return;
+            //Si todavía no la encuentra, buscará la clase en las rutas de la raíz WIKI_IOC_MODEL
+            foreach ($arr_dir_class as $dir) {
+                $class_file = WIKI_IOC_MODEL.$dir.$name.".php";
+                if (@file_exists($class_file) && is_file($class_file)) {
+                    include_once ($class_file);
+                    return;
+                }
             }
+        }else {
+            getAuthorizationClass($name);
         }
     }
     return;
 }
 
-/* Establece las rutas propias ($projectdir) de las clases del proyecto actual */
+//Carga la clase de autorización por defecto
+function getAuthorizationClass($name) {
+    $matches = [];
+    if (preg_match('/(.*)(Authorization)$/', $name, $matches)) {
+        if (is_file(DOKU_LIB_IOC."wikiiocmodel/authorization/{$matches[0]}.php")) {
+            require_once(DOKU_LIB_IOC."wikiiocmodel/authorization/{$matches[0]}.php");
+            return TRUE;
+        }
+    }
+}
+
+// Establece las rutas propias ($projectdir) de las clases del proyecto actual
 function getDirClass($name) {
    $cfg = array (
             "Action" => array (
